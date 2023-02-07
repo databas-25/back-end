4f6cc0e0e2aa67997496aa2668a49b5cdde8f547
Simon Engström <simonspost2012@gmail.com>
2023-02-07 15:47:21 +0000

    made more route files

---
+++ src/routes/cart.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const pool = require('../setup');
+const router = express.Router();
+
+
+
+
+
+module.exports = router;
---
+++ src/routes/order.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const pool = require('../setup');
+const router = express.Router();
+
+
+
+
+
+
+
+module.exports = router;
