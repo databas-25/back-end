dbd7c8f01369aa8b5355ee1d414be42ba32a1cfd
Dayrren <isak.forsgren@gmail.com>
2023-02-10 13:53:31 +0000

    bob

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,24 +1,22 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 const pool = require('../setup');
+
 const router = express.Router();
 
 router.get('/fetch_items', (req, res) => {
-    try {
-        //const claims = jwt.verify(req.body.token, process.env.ACCESS_TOKEN_SECRET, jwtOptions);
-        const rows = [];
-        pool.query('SELECT Products_Product_id, amount FROM Basket_Items WHERE Users_User_id = ?', [req.body.userId])
-            .on('result', rows.push).on('end', (r) => {
-                res.send({success: true, data: rows});
-            });
-    } catch(e) {
-        res.send({
-            success: false
-        });
-    }
+	try {
+		// const claims = jwt.verify(req.body.token, process.env.ACCESS_TOKEN_SECRET, jwtOptions);
+		const rows = [];
+		pool.query('SELECT Products_Product_id, amount FROM Basket_Items WHERE Users_User_id = ?', [req.body.userId])
+			.on('result', rows.push).on('end', () => {
+				res.send({ success: true, data: rows });
+			});
+	} catch (e) {
+		res.send({
+			success: false,
+		});
+	}
 });
-
-
 
 module.exports = router;
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const pool = require('../setup');
+
 const router = express.Router();
-
-
-
-
-
-
-
 
 module.exports = router;
