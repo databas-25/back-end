df5e234e608b70d893cfc483db8f1e9ffe9a922a
Dayrren <isak.forsgren@gmail.com>
2023-02-10 15:09:32 +0000

    beeg sql

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,12 +4,32 @@ const pool = require('../setup');
 
 const router = express.Router();
 
-router.get('/fetch_items', (req, res) => {
+router.post('/fetch_items', (req, res) => {
 	try {
+		console.log(req.body);
+
 		// const claims = jwt.verify(req.body.token, process.env.ACCESS_TOKEN_SECRET, jwtOptions);
+
+		const sql = ' SELECT'
+			+ ' Products.product_name,'
+			+ ' Products.price,'
+			+ ' Basket_Items.Products_Product_id,'
+			+ ' Basket_Items.amount'
+			+ ' FROM'
+			+ ' 	Basket_Items'
+			+ ' LEFT JOIN Products ON Basket_Items.Products_Product_id = Products.Product_id'
+			+ ' WHERE'
+			+ ' 	Basket_Items.Users_User_id = ?;';
+
 		const rows = [];
-		pool.query('SELECT Products_Product_id, amount FROM Basket_Items WHERE Users_User_id = ?', [req.body.userId])
-			.on('result', rows.push).on('end', () => {
+		console.log(sql);
+		pool.query(sql, [req.body.userId])
+			.on('result', (r) => {
+				console.log(r);
+				rows.push(r);
+			})
+			.on('end', () => {
+				console.log(rows);
 				res.send({ success: true, data: rows });
 			});
 	} catch (e) {
