ee7302d2209cab5c20890c868ecbb9a5fbe129c7
Simon Engström <simonspost2012@gmail.com>
2023-02-10 13:49:45 +0000

    added cart fetch

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const pool = require('../setup');
 const router = express.Router();
 
-
+router.get('/fetch_items', (req, res) => {
+    try {
+        //const claims = jwt.verify(req.body.token, process.env.ACCESS_TOKEN_SECRET, jwtOptions);
+        const rows = [];
+        pool.query('SELECT Products_Product_id, amount FROM Basket_Items WHERE Users_User_id = ?', [req.body.userId])
+            .on('result', rows.push).on('end', (r) => {
+                res.send({success: true, data: rows});
+            });
+    } catch(e) {
+        res.send({
+            success: false
+        });
+    }
+});
 
 
 
