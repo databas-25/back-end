52cd9f42f0a2c6fad6b92f5c40cf4f3fb52c3ad6
Simon Engström <simonspost2012@gmail.com>
2023-03-01 13:24:54 +0000

    review files

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const userRouter = require('./routes/user');
 const cartRouter = require('./routes/cart');
 const orderRouter = require('./routes/order');
 const productRouter = require('./routes/product');
+const reviewRouter = require('./routes/review');
 
 const PORT = 8000;
 
@@ -19,6 +20,7 @@ app.use('/user', userRouter);
 app.use('/cart', cartRouter);
 app.use('/order', orderRouter);
 app.use('/product', productRouter);
+app.use('/review', reviewRouter);
 
 app.post('/hello', (req, res) => {
 	res.send({
